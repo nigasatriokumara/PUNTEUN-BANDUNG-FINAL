01611e76dda1257adcb6e21c5a1e0b7cd5601233
nigasatriokumara <nigakumara@gmail.com>
2019-04-02 05:18:17 +0700

    Admin Register

---
+++ app/src/main/java/android/example/com/punteunbandungfinal/AdminRegister.java
@@ -0,0 +1,13 @@
+package android.example.com.punteunbandungfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_register);
+    }
+}
